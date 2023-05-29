0f3a3635686313d46e51d0b10c1b129a8411ebc8
K3nK0 <uncommonmen@hotmail.fr>
2023-05-29 23:42:45 +0200

    gestion des erreurs

--- app.js
+++ app.js
@@ -1,65 +1,159 @@
 // Attend que le document soit chargé
 document.addEventListener('DOMContentLoaded', function() {
 
-    const inputDay = document.getElementById('input-day')
-    const labelDay = document.getElementById('label-day')
-    const inputMonth = document.getElementById('input-month')
-    const labelMonth = document.getElementById('label-month')
-    const inputYear = document.getElementById('input-year')
-    const labelYear = document.getElementById('label-year')
+  // Sélectionne le formulaire
+  const form = document.querySelector('form');
 
-    // Sélectionne le formulaire
-    const form = document.querySelector('form');
-    
-    // Ajoute un écouteur d'événement pour la soumission du formulaire
-    form.addEventListener('submit', function(e) {
-      e.preventDefault(); // Empêche le rechargement de la page
+  // Récupère la date actuelle
+  let currentDate = new Date();
+  let currentYear = currentDate.getFullYear();
+  let currentMonth = currentDate.getMonth() + 1; // Les mois vont de 0 à 11
+  let currentDay = currentDate.getDate();
+
+  const day = document.getElementById('day');
+  const labelDay = document.getElementById('label-day');
+  const textErrorDay = document.getElementById('error-day');
+
+  const month = document.getElementById('month');
+  const labelMonth = document.getElementById('label-month');
+  const textErrorMonth = document.getElementById('error-month');
+
+  const year = document.getElementById('year');
+  const labelYear = document.getElementById('label-year');
+  const textErrorYear = document.getElementById('error-year');
+
+
+  // Ajoute un écouteur d'événement pour la soumission du formulaire
+  form.addEventListener('submit', (e) => {
+
+    document.getElementById('years').innerText = "--";
+    document.getElementById('months').innerText = "--";
+    document.getElementById('days').innerText = "--";
+
+    textErrorDay.innerText = "";
+    labelDay.style.color = "hsl(0, 1%, 44%)";
+    day.style.border = "1px solid hsl(0, 1%, 44%)";
+
+    textErrorMonth.innerText = "";
+    labelMonth.style.color = "hsl(0, 1%, 44%)";
+    month.style.border = "1px solid hsl(0, 1%, 44%)";
+
+    textErrorYear.innerText = "";
+    labelYear.style.color = "hsl(0, 1%, 44%)";
+    year.style.border = "1px solid hsl(0, 1%, 44%)";
+
+    e.preventDefault(); // Empêche le rechargement de la page
+    checkDay();
+    checkMonth();
+    checkYear();
       
-      // Récupère les valeurs saisies par l'utilisateur
-      let day = parseInt(document.getElementById('day').value);
-      let month = parseInt(document.getElementById('month').value);
-      let year = parseInt(document.getElementById('year').value);
-      
-      // Vérifie si les valeurs sont valides
-      if (day === "") {
-        
-      }
-      
-      // Récupère la date actuelle
-      let currentDate = new Date();
-      let currentYear = currentDate.getFullYear();
-      let currentMonth = currentDate.getMonth() + 1; // Les mois vont de 0 à 11
-      let currentDay = currentDate.getDate();
-      
+    let checkDayResult = checkDay()
+    console.log(checkDayResult);
+
+    if (checkDay() === false && checkMonth() === false && checkYear() === false ){
+
+      const dayValue = parseInt(day.value)
+      const monthValue = parseInt(month.value)
+      const yearValue = parseInt(year.value)
+
       // Calcul de l'âge en années, mois et jours
-      let ageYears = currentYear - year;
-      let ageMonths = currentMonth - month;
-      let ageDays = currentDay - day;
-      
+      let ageYears = currentYear - yearValue;
+      let ageMonths = currentMonth - monthValue;
+      let ageDays = currentDay - dayValue;
+
       // Si le mois actuel est inférieur au mois de naissance, ajuste l'âge en conséquence
-      if (currentMonth < month) {
+      if (currentMonth < monthValue) {
         ageYears--;
-        ageMonths = 12 - (month - currentMonth);
+        ageMonths = 12 - (monthValue - currentMonth);
       }
-      
+
       // Si le mois actuel est égal au mois de naissance, vérifie le jour actuel
-      if (currentMonth === month) {
-        // Si le jour actuel est inférieur au jour de naissance, ajuste l'âge en conséquence
-        if (currentDay < day) {
+      if (currentMonth === monthValue) {
+        if (currentDay > dayValue) {
           ageYears--;
           ageMonths = 11;
           let daysInPreviousMonth = new Date(currentYear, currentMonth - 1, 0).getDate();
-          ageDays = daysInPreviousMonth - (day - currentDay);
-        } else if (currentDay === day) {
-          // Si le jour actuel est égal au jour de naissance, affiche un message spécial
-          alert('Joyeux anniversaire !');
+          let remainingDays = daysInPreviousMonth - (dayValue - currentDay);
+          ageDays = remainingDays < 0 ? daysInPreviousMonth : remainingDays;
+        } else if (currentDay > dayValue) {
+          ageDays = currentDay - dayValue;
+        } else {
+          ageDays = 0;
         }
       }
-      
+
       // Affiche les résultats dans l'interface utilisateur
       document.getElementById('years').textContent = ageYears;
       document.getElementById('months').textContent = ageMonths;
       document.getElementById('days').textContent = ageDays;
-    });
+
+    }
+
   });
+
+  function checkDay(){
+    if(day.value === ""){
+      textErrorDay.innerText = "This field is required";
+      labelDay.style.color = "hsl(0, 100%, 67%)";
+      day.style.border = "1px solid hsl(0, 100%, 67%)";
+    }
+    else if(!(1 <= day.value && day.value <= 31)){
+      textErrorDay.innerText = "Must be a valid day";
+      labelDay.style.color = "hsl(0, 100%, 67%)";
+      day.style.border = "1px solid hsl(0, 100%, 67%)";
+    }
+    else{
+      const monthValue = parseInt(month.value);
+      const yearValue = parseInt(year.value);
+      const maxDays = new Date(yearValue, monthValue, 0).getDate();
+
+      if (day.value > maxDays){
+        textErrorDay.innerText = "Must be a valid date";
+        labelDay.style.color = "hsl(0, 100%, 67%)";
+        day.style.border = "1px solid hsl(0, 100%, 67%)";
+        labelMonth.style.color = "hsl(0, 100%, 67%)";
+        month.style.border = "1px solid hsl(0, 100%, 67%)";
+        labelYear.style.color = "hsl(0, 100%, 67%)";
+        year.style.border = "1px solid hsl(0, 100%, 67%)";
+      }
+      else{
+        return false;
+      }
+    }
+  }
+
+  function checkMonth(){
+    if(month.value === ""){
+      textErrorMonth.innerText = "This field is required";
+      labelMonth.style.color = "hsl(0, 100%, 67%)";
+      month.style.border = "1px solid hsl(0, 100%, 67%)";
+    }
+    else if(!(1 <= month.value && month.value <= 12)){
+      textErrorMonth.innerText = "Must be a valid month";
+      labelMonth.style.color = "hsl(0, 100%, 67%)";
+      month.style.border = "1px solid hsl(0, 100%, 67%)";
+    }
+    else{
+      return false;
+    }
+  }
+
+  function checkYear(){
+    if(year.value === ""){
+      textErrorYear.innerText = "This field is required";
+      labelYear.style.color = "hsl(0, 100%, 67%)";
+      year.style.border = "1px solid hsl(0, 100%, 67%)";
+    }
+    else if(year.value > currentYear){
+      textErrorYear.innerText = "Must be a valid year";
+      labelYear.style.color = "hsl(0, 100%, 67%)";
+      year.style.border = "1px solid hsl(0, 100%, 67%)";
+    }
+    else{
+      return false;
+    }
+  }
+
+  });
+  
   
